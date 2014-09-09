cd84224d156d7fd76c0d29505af23b9941fca1ea
jeffgagnon01 <jeffgagnon_01@hotmail.com>
2014-09-09 13:06:33 -0400

    add ex02

---
+++ bin/ex02.dart
@@ -0,0 +1,30 @@
+//je n'arrive pas à importer la libraire avec import dart:math; 
+
+//Question 1
+void main() {
+  var x = 5;
+  var y = 7;
+  var z = 8;
+print(x-y/z);  // Answer
+  
+//Question 2
+  var r = 10;
+  num pi = 3.14159; 
+  const double PI = 3.1415926535897932;  //copié de la librairie dart:math
+  
+      print(PI*r*r); //Answer   Comment faire un exposant en informatique ? ^ ^
+      
+//Question 3
+var A= 220;
+var B= 260;
+  print('sqft = $A*$B'); // 1 acre = 43 560 pieds carrés
+const acre = 43560;  
+  print((A*B)/acre); //answer
+  
+// Question 4
+var time = 9.58; // tiré des notes de cours
+const dist = 100;
+var vit = 100/9.58;  // peut-on remplacer les valeurs numeriques par les variables directement ?
+print('vitesse(m/s) = $vit');  //answer
+
+}
